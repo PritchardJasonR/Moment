366cf57252b7e993873f7722f124cc5dc5712288
PritchardJasonR <johndoe@example.coPritchardJasonR@gmail.com>
2019-02-24 23:37:41 -0700

    More photos and better commenting

--- testAssets/fillData.js
+++ testAssets/fillData.js
@@ -1,31 +1,31 @@
 module.exports = {
 
     // Fill Fields for Account Creation
-    userName: 'Alex',
-    firstName: 'Alex',
-    lastName: 'Grace',
-    password: '12345678',  // Minimum 8 Char
-    email: 'Alex@webb.com',
-    phone:'7145619363',
-    createLocation:'Deep Space' ,
-    gender: 'Male', // Word
-    textArea:'Shelby they are coming!' ,
-    photo: 'userPhoto.jpg',
-    hostLocation: 'Waikiki Beach', 
-    category: 'Nature',
-    qualifications: 'I love Waikiki and return many times yearly!', 
-    whatDo: 'Eat, Drink , Swim, Dance!!!', 
-    whereBe: 'All over the dang place!, Let`s see where the wind takes us >.<' ,
-    hostTitle: 'WaiKiKi Eat,Swim,Drink,Dance',
-    photo1: 'host11.jpg',
-    photo2: 'host12.jpg',
-    hostAddress: '151 Kapahulu Ave',
-    hostCity: 'Honolulu',
-    hostState: 'HI',
-    hostZip: '96815',
-    hostAddressTitle: 'WaiKiKi Zoo',
-    groupSize: '4',
-    price: '20',
+    userName: 'User Name',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    password: 'Password',  // Minimum 8 Char
+    email: 'Email',
+    phone:'Phone',  // 10 digits
+    createLocation:'Location',
+    gender: 'Male', // Word (Male, Female, Prefer not to answer)
+    textArea: 'Users Description',
+    photo: 'File Name', // userPhoto(1-5).jpg
+    hostLocation: 'Location for Event', 
+    category: 'Category',  //Exact:  Arts & design, Fashion, Entertainment, Sports, Wellness, Nature, Food & Drink, Lifestyle, Music, History, Business, Nightlife
+    qualifications: 'Qualification', 
+    whatDo: 'Describe', // Describe what you will do on the adventure
+    whereBe: 'Describe' , // Describe where you'll go on the adventure
+    hostTitle: 'Title', // Title of the adventure
+    photo1: 'File Name', // host(11,21,31,41,51,).jpg
+    photo2: 'File Name',// host(12,22,32,42,52 ).jpg
+    hostAddress: 'Address',
+    hostCity: 'City',
+    hostState: 'State',
+    hostZip: 'Zip',
+    hostAddressTitle: 'Name of place',
+    groupSize: 'Number', // 1 - 10
+    price: 'Number', // decimals will be rounded up
 
 
     
